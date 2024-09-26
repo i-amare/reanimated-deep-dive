966abd386e050e19b7db9b394b372a1d8a020b76
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-26 17:53:07 +0200

    first animation

--- app/(tabs)/Screen-1/index.tsx
+++ app/(tabs)/Screen-1/index.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from 'react-native';
+import { Pressable, View } from 'react-native';
+import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
 
 export default function Screen1() {
   return (
-    <View className='h-screen w-full flex-1 bg-red-400'>
-      <Text>Test</Text>
+    <View className='h-screen w-full flex-1 items-center justify-evenly bg-red-400'>
+      <Block1 />
     </View>
+  );
+}
+
+function Block1() {
+  const [width, height] = [useSharedValue(50), useSharedValue(50)];
+
+  function onBlockPress() {
+    [width.value, height.value] = [
+      withSpring(width.value + 50),
+      withSpring(height.value + 50),
+    ];
+  }
+  return (
+    <Pressable onPress={onBlockPress}>
+      <Animated.View
+        style={{ width: width, height: height }}
+        className='rounded-lg bg-blue-500'
+      ></Animated.View>
+    </Pressable>
   );
 }
