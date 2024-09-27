e5962e86ea5a3f8ca91da1ae126fc30f893ec98c
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-27 22:03:26 +0200

    failed to use animated props :(

--- app/(tabs)/Screen-1/index.tsx
+++ app/(tabs)/Screen-1/index.tsx
@@ -1,15 +1,39 @@
+import React, { forwardRef } from 'react';
 import { Pressable, View } from 'react-native';
 import Animated, {
+  SharedValue,
+  useAnimatedProps,
   useAnimatedStyle,
   useSharedValue,
   withSpring,
+  withTiming,
 } from 'react-native-reanimated';
 
+const AnimatedBlock3 = Animated.createAnimatedComponent(
+  forwardRef(({ width, height, onPress }: Block3Props, ref) => (
+    <Block3 width={width} height={height} onPress={onPress} ref={ref} />
+  )),
+);
+
 export default function Screen1() {
+  const width = useSharedValue(50);
+  const height = useSharedValue(50);
+
+  const animatedProps = useAnimatedProps(() => ({
+    width: withTiming(width.value),
+    height: withTiming(height.value),
+  }));
+
+  function onBlock3Press() {
+    width.value += 50;
+    height.value += 50;
+  }
+
   return (
     <View className='h-screen w-full flex-1 items-center justify-evenly bg-gray-950'>
       <Block1 />
       <Block2 />
+      <AnimatedBlock3 width={width} height={height} onPress={onBlock3Press} />
     </View>
   );
 }
@@ -54,7 +78,30 @@ function Block2() {
     <Pressable onPress={onBlockPress}>
       <Animated.View
         style={[animatedStyle]}
-        className='bg-pink-300 rounded-lg'
+        className='rounded-lg bg-pink-300'
+      ></Animated.View>
+    </Pressable>
+  );
+}
+
+interface Block3Props {
+  onPress: () => void;
+  width?: SharedValue<number>;
+  height?: SharedValue<number>;
+  ref: React.ForwardedRef<any>;
+}
+
+function Block3({ width, height, ref, onPress }: Block3Props) {
+  const animatedStyle = useAnimatedStyle(() => ({
+    width: withSpring(width?.value || 50),
+    height: withSpring(height?.value || 50),
+  }));
+
+  return (
+    <Pressable onPress={onPress} ref={ref}>
+      <Animated.View
+        style={[animatedStyle]}
+        className='rounded-lg bg-pink-300'
       ></Animated.View>
     </Pressable>
   );
