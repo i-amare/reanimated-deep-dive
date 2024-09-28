6d78e423713c965ac79d030a3d9b726210709018
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-28 11:22:30 +0200

    withTiming InOut easing

--- app/(tabs)/Screen-2/index.tsx
+++ app/(tabs)/Screen-2/index.tsx
@@ -14,8 +14,9 @@ const BLOCK_TRANSLATION = 100;
 export default function Screen2() {
   useEffect(() => {
     const interval = setInterval(() => {
-      xTranslation.value = xTranslation.value >= 0 ? -BLOCK_TRANSLATION : BLOCK_TRANSLATION;
-    }, 1000);
+      xTranslation.value =
+        xTranslation.value >= 0 ? -BLOCK_TRANSLATION : BLOCK_TRANSLATION;
+    }, 1500);
     return () => clearInterval(interval);
   });
 
@@ -25,6 +26,7 @@ export default function Screen2() {
     <View className='bg-grey-950 flex-1 items-center justify-evenly'>
       <Block1 xTranslation={xTranslation} />
       <Block2 xTranslation={xTranslation} />
+      <Block3 xTranslation={xTranslation} />
     </View>
   );
 }
@@ -33,7 +35,7 @@ interface Block1Props {
   xTranslation: SharedValue<number>;
 }
 
-function Block1({xTranslation}: Block1Props) {
+function Block1({ xTranslation }: Block1Props) {
   const animatedStyle = useAnimatedStyle(() => ({
     transform: [{ translateX: withSpring(xTranslation.value) }],
   }));
@@ -46,23 +48,50 @@ function Block1({xTranslation}: Block1Props) {
   );
 }
 
-
 interface Block2Props {
   xTranslation: SharedValue<number>;
 }
 
-function Block2({xTranslation}: Block2Props) {
+function Block2({ xTranslation }: Block2Props) {
   const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ translateX: withTiming(xTranslation.value, {
-      duration: 900,
-      easing: Easing.bounce
-    }) }],
+    transform: [
+      {
+        translateX: withTiming(xTranslation.value, {
+          duration: 1000,
+          easing: Easing.bounce,
+        }),
+      },
+    ],
   }));
 
   return (
     <Animated.View
       style={animatedStyle}
       className='aspect-square w-[50px] rounded-lg bg-purple-400'
+    ></Animated.View>
+  );
+}
+
+interface Block3Props {
+  xTranslation: SharedValue<number>;
+}
+
+function Block3({ xTranslation }: Block3Props) {
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: withTiming(xTranslation.value, {
+          duration: 1000,
+          easing: Easing.inOut(Easing.quad),
+        }),
+      },
+    ],
+  }));
+
+  return (
+    <Animated.View
+      style={animatedStyle}
+      className='aspect-square w-[50px] rounded-lg bg-yellow-400'
     ></Animated.View>
   );
 }
