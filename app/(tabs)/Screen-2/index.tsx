476762baf87f1e03771c3723b08cfa1af2800101
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-28 11:07:18 +0200

    withSpring animations

--- app/(tabs)/Screen-2/index.tsx
+++ app/(tabs)/Screen-2/index.tsx
@@ -1,9 +1,39 @@
-import { Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { View } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
 
 export default function Screen2() {
   return (
-    <View className='flex-1 bg-blue-400'>
-      <Text>Screen 2</Text>
+    <View className='bg-grey-950 flex-1 items-center justify-evenly'>
+      <Block1 />
     </View>
+  );
+}
+
+function Block1() {
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (xTranslation.value >= 0) xTranslation.value = -100;
+      else xTranslation.value = 100;
+      console.log('triggered');
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const xTranslation = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: withSpring(xTranslation.value) }],
+  }));
+
+  return (
+    <Animated.View
+      style={animatedStyle}
+      className='aspect-square w-[50px] rounded-lg bg-red-400'
+    ></Animated.View>
   );
 }
