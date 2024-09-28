9d0b6b877a307546d975e065fa1ccfdb5cadb121
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-28 16:04:44 +0200

    long press gesture

--- app/(tabs)/Screen-4/index.tsx
+++ app/(tabs)/Screen-4/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View } from 'react-native';
+import { Easing, View } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
   useAnimatedStyle,
@@ -10,6 +10,15 @@ import Animated, {
 } from 'react-native-reanimated';
 
 export default function Screen4() {
+  return (
+    <View className='flex-1 items-center justify-evenly bg-gray-950'>
+      <Block1 />
+      <Block2 />
+    </View>
+  );
+}
+
+function Block1() {
   const [shape, setShape] = useState<'square' | 'circle'>('square');
 
   const tap = Gesture.Tap().onBegin(onTapStart).onFinalize(onTapEnd);
@@ -18,12 +27,10 @@ export default function Screen4() {
     if (shape == 'square') {
       borderRadius.value = 50;
       scale.value = 1.5;
-      rotation.value = 180;
       setShape('circle');
     } else {
       borderRadius.value = 8;
       scale.value = 1;
-      rotation.value = 0;
       setShape('square');
     }
     console.log('Tap Started');
@@ -35,7 +42,6 @@ export default function Screen4() {
 
   const borderRadius = useSharedValue(8);
   const scale = useSharedValue(1);
-  const rotation = useSharedValue(0);
 
   const animatedStyle = useAnimatedStyle(() => ({
     borderRadius: withTiming(borderRadius.value, { duration: 2000 }),
@@ -53,13 +59,48 @@ export default function Screen4() {
   }));
 
   return (
-    <View className='flex-1 scale-90 items-center justify-evenly bg-gray-950'>
-      <GestureDetector gesture={tap}>
-        <Animated.View
-          style={animatedStyle}
-          className='aspect-square w-[100px] rounded-lg bg-green-400'
-        ></Animated.View>
-      </GestureDetector>
-    </View>
+    <GestureDetector gesture={tap}>
+      <Animated.View
+        style={animatedStyle}
+        className='aspect-square w-[100px] rounded-lg bg-green-400'
+      ></Animated.View>
+    </GestureDetector>
+  );
+}
+
+const SCALE_INCREMENT_VALUE = 1.1;
+
+function Block2() {
+  const scale = useSharedValue(1);
+
+  let timer: NodeJS.Timeout;
+  const hold = Gesture.LongPress()
+    .onBegin(() => {
+      timer = setInterval(increaseScale, 100);
+    })
+    .onFinalize(() => {
+      scale.value = withSpring(1);
+      clearInterval(timer);
+    });
+
+  function increaseScale() {
+    if (scale.value >= 4) return;
+    scale.value = withTiming(scale.value * SCALE_INCREMENT_VALUE, {
+      duration: 100,
+      easing: Easing.linear,
+    });
+  }
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+  }));
+
+  return (
+    <GestureDetector gesture={hold}>
+      <Animated.View
+        style={animatedStyles}
+        className='aspect-square w-[100px] rounded-full bg-orange-400'
+      ></Animated.View>
+    </GestureDetector>
   );
 }
